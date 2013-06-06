96a35051a6996fe6a2e12adcd7519b8e8cc33964
Jon Thornton <jthornton@solstice-consulting.com>
2013-06-06 08:13:50 -0500

    Changed the theme of the app and began working on the contact list item.

--- TextMessenger/src/com/thornton/textmessenger/MessageActivity.java
+++ TextMessenger/src/com/thornton/textmessenger/MessageActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 
 import com.thornton.textmessenger.conversation.ContactSelectorFragment;
 
-public class MessageActivity extends FragmentActivity{
+public class MessageActivity extends FragmentActivity {
 
 	private SlidingPaneLayout mSlidingLayout;
 	private ActionBar mActionBar;
@@ -22,7 +22,8 @@ public class MessageActivity extends FragmentActivity{
 		setContentView(R.layout.activity_message);
 		final ContactSelectorFragment fragment = new ContactSelectorFragment();
 
-		this.getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
 
 		setUpActionBar();
 	}
@@ -34,44 +35,62 @@ public class MessageActivity extends FragmentActivity{
 		 * closed, as the left pane contains content one level up in the
 		 * navigation hierarchy.
 		 */
-		if ((item.getItemId() == android.R.id.home) && !mSlidingLayout.isOpen()) {
-			mSlidingLayout.openPane();
-			return true;
-		}else if ((item.getItemId() == android.R.id.home) && mSlidingLayout.isOpen()) {
-			mSlidingLayout.closePane();
+		if (item.getItemId() == android.R.id.home) {
+			toggleDrawer();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	/**
+	 * Toggle the sliding drawer. If the drawer is open, close it or vice versa
+	 */
+	protected void toggleDrawer() {
+		if (mSlidingLayout.isOpen()) {
+			mSlidingLayout.closePane();
+		} else {
+			mSlidingLayout.openPane();
+		}
+	}
+
 	@SuppressLint("NewApi")
-	private void setUpActionBar(){
+	private void setUpActionBar() {
 		mActionBar = getActionBar();
 		mSlidingLayout = (SlidingPaneLayout) findViewById(R.id.sliding_pane_layout);
 
 		mActionBar.setHomeButtonEnabled(true);
 		mActionBar.setDisplayHomeAsUpEnabled(true);
-		mSlidingLayout.setPanelSlideListener(new PanelSlideListener(){
+		mSlidingLayout.setPanelSlideListener(new PanelSlideListener() {
 
 			@Override
 			public void onPanelOpened(final View panel) {
 				if (mSlidingLayout.isSlideable()) {
-					getSupportFragmentManager().findFragmentById(R.id.content_frame).setHasOptionsMenu(false);
-					getSupportFragmentManager().findFragmentById(R.id.list_pane).setHasOptionsMenu(true);
+					getSupportFragmentManager().findFragmentById(
+							R.id.content_frame).setHasOptionsMenu(false);
+					getSupportFragmentManager()
+							.findFragmentById(R.id.list_pane)
+							.setHasOptionsMenu(true);
 				} else {
-					getSupportFragmentManager().findFragmentById(R.id.content_frame).setHasOptionsMenu(true);
-					getSupportFragmentManager().findFragmentById(R.id.list_pane).setHasOptionsMenu(false);
+					getSupportFragmentManager().findFragmentById(
+							R.id.content_frame).setHasOptionsMenu(true);
+					getSupportFragmentManager()
+							.findFragmentById(R.id.list_pane)
+							.setHasOptionsMenu(false);
 				}
 			}
 
 			@Override
 			public void onPanelClosed(final View panel) {
-				getSupportFragmentManager().findFragmentById(R.id.content_frame).setHasOptionsMenu(true);
-				getSupportFragmentManager().findFragmentById(R.id.list_pane).setHasOptionsMenu(false);
+				getSupportFragmentManager()
+						.findFragmentById(R.id.content_frame)
+						.setHasOptionsMenu(true);
+				getSupportFragmentManager().findFragmentById(R.id.list_pane)
+						.setHasOptionsMenu(false);
 			}
 
 			@Override
-			public void onPanelSlide(final View view, final float v) {}
+			public void onPanelSlide(final View view, final float v) {
+			}
 
 		});
 	}
